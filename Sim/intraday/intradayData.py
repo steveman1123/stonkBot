09d30c25981907f4ba656b26607ca8afcada120c
Steve <smw1109@protonmail.com>
2020-08-15 22:17:33 -0500

    rm old files

--- Sim/intraday/intradayData.py
+++
@@ -1,7 +0,0 @@
-import requests
-from datetime import date
-symb = "XSPA"
-a = requests.get("https://www.alphavantage.co/query",params={"function":"TIME_SERIES_INTRADAY","symbol":symb,"interval":"1min","outputsize":"full","apikey":"","datatype":"csv"}).content
-f = open(symb+"_"+str(date.today())+".csv","w")
-f.write(str(a).replace("\\r\\n","\n"))
-f.close()
